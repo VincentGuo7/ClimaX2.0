4f30987f16eb8b2f4330ef59c1a899577ac42eb1
Vincent Guo <Vincent.Guo@accenture.com>
2025-06-18 13:56:45 +1000

    Fixing crash

--- src/climax/regional_forecast/train.py
+++ src/climax/regional_forecast/train.py
@@ -58,14 +58,15 @@ def main():
         cli.trainer.fit(cli.model, datamodule=cli.datamodule)
         print("🚀 Starting training from scratch (no checkpoint found).")
 
+    cli.trainer.test(cli.model, datamodule=cli.datamodule, ckpt_path="best")
 
-    # Run test and save results
-    test_results = cli.trainer.test(cli.model, datamodule=cli.datamodule, ckpt_path="best")
+    # # Run test and save results
+    # test_results = cli.trainer.test(cli.model, datamodule=cli.datamodule, ckpt_path="best")
     
-    os.makedirs(f"{cli.trainer.default_root_dir}/metrics", exist_ok=True)
-    results_path = os.path.join(cli.trainer.default_root_dir, "metrics", "test_metrics_3days_10epochs.csv")
-    pd.DataFrame(test_results).to_csv(results_path, index=False)
-    print(f"\n✅ Test metrics saved to: {results_path}")
+    # os.makedirs(f"{cli.trainer.default_root_dir}/metrics", exist_ok=True)
+    # results_path = os.path.join(cli.trainer.default_root_dir, "metrics", "test_metrics_3days_10epochs.csv")
+    # pd.DataFrame(test_results).to_csv(results_path, index=False)
+    # print(f"\n✅ Test metrics saved to: {results_path}")
 
 
 if __name__ == "__main__":
